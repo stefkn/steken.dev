cfb766fabd4ea4eebab7d77c8a8c101dff60c660
Stefan Nowak <stefan.nowak@tails.com>
2021-02-23 23:52:27 +0000

    import keyframes

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { Section } from '@components/global';
 
