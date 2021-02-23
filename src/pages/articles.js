62cd1323735be0eef0954424c3140573de8ffd94
Stefan Nowak <stefan.nowak@tails.com>
2021-02-23 23:53:51 +0000

    refactor articles component to use tags

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -2,32 +2,76 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '@common/Layout';
-import { graphql } from 'gatsby'
+import { StaticQuery, graphql } from 'gatsby'
 
 import Footer from '@sections/Footer';
 import Navbar from '@common/Navbar';
 
 import styled from 'styled-components';
 import PostLink from "../components/common/PostLink"
+import PostTag from "../components/common/PostTag"
 
-const Articles = ({
-  data: {
-    allMarkdownRemark: { edges },
-  },
-}) => {
-  const Posts = edges
-    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
-  return <Layout>
-    <Navbar />
-    <MainMatter>
-      <h1 className="title-main">articles</h1>
-      <h2 className="subtitle-main">Here are some of my collections of words and other things:</h2>
-      <div>{Posts}</div>
-      <Link className="home-link" to="/">Go back to the homepage</Link>
-    </MainMatter>
-    <Footer bottomImage={false}/>
-  </Layout>
+
+export default function Articles() {
+  let state = {
+    selectedTag: '',
+  };
+
+  const selectTag = (tag) => {
+    this.setState({selectedTag: tag});
+  }
+
+  const selectNoTags = () => {
+    this.setState({selectedTag: ''})
+  }
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+            edges {
+              node {
+                id
+                excerpt(pruneLength: 250)
+                frontmatter {
+                  date(formatString: "MMMM DD, YYYY")
+                  slug
+                  title
+                  tags
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        const response = data.allMarkdownRemark.edges
+        let Posts = <div>nothing yet</div>;
+        let Tags = <div>nothing here</div>;
+        if (!!response) {
+          Posts = response
+            .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+            .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+          Tags = response
+            .map(edge => <PostTag key={edge.node.id} post={edge.node} />)
+        }
+        return (
+        <Layout>
+        <Navbar />
+        <MainMatter>
+          <h1 className="title-main">articles</h1>
+          <h2 className="subtitle-main">Here are some of my collections of words and other things:</h2>
+          <div>{Tags}</div>
+          <div>{Posts}</div>
+          <Link className="home-link" to="/">Go back to the homepage</Link>
+        </MainMatter>
+        <Footer bottomImage={false}/>
+        </Layout>
+        )
+      }}
+    />
+  )
 }
 
 const MainMatter = styled.div`
@@ -49,22 +93,4 @@ const MainMatter = styled.div`
   }
 `;
 
-export default Articles;
-
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            slug
-            title
-          }
-        }
-      }
-    }
-  }
-`
+// export default Articles;
