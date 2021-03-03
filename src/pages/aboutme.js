04775ca1b1c51ce0a4b9df434e58a82787ccd28f
Stefan Nowak <stefan.nowak@tails.com>
2021-03-03 23:07:25 +0000

    update aboutme page with more stuff

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -10,15 +10,55 @@ import Navbar from '@common/Navbar';
 import styled from 'styled-components';
 
 const AboutMe = () => (
-  <Layout>
-    <Navbar />
-    <MainMatter className="page-main">
-      <h1 className="title-main">about </h1>
-      <p className="subtitle-main">Stefan Nowak is a (he/him) from London, England. He is &#189; Japanese, &#188; Latvian, &#188; Polish and 100% <a href="https://www.youtube.com/watch?v=8Dd_qiuWxPs">meat popsicle.</a></p>
-      <Link className="home-link" to="/">Go back to the homepage</Link>
-    </MainMatter>
-    <Footer bottomImage={false} />
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query {
+        stefan_img: file(
+          sourceInstanceName: { eq: "art" }
+          name: { eq: "me2" }
+        ) {
+          childImageSharp {
+            fluid(maxWidth: 673) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+    <Layout>
+      <Navbar />
+      <MainMatter className="page-main">
+        <Art><Img fluid={data.stefan_img.childImageSharp.fluid} /></Art>
+        <h1 className="title-main">about this guy</h1>
+        <p className="subtitle-main">Stefan Nowak is a (he/him) from 🇬🇧 London, England.</p>
+        <p className="text">He is &#189; 🇯🇵 Japanese, &#188; 🇱🇻 Latvian, &#188; 🇵🇱 Polish.</p>
+        <p className="text">He grew up in south London, and went to school in Sutton.</p>
+        <p className="text">He is a master procrastinator, and it's a wonder that he really gets anything done at all.</p>
+        <Grid>
+          <Card>
+            <h3 className="title-main">resumé &rarr;</h3>
+            <p>view my CV</p>
+          </Card>
+          <Card>
+            <h3 className="title-main">tech &rarr;</h3>
+            <p>technologies I've used</p>
+          </Card>
+          <Card>
+            <h3 className="title-main">projects &rarr;</h3>
+            <p>some of my work</p>
+          </Card>
+          <Card>
+            <h3 className="title-main">photos &rarr;</h3>
+            <p>pictures I've taken</p>
+          </Card>
+        </Grid>
+        <Link className="home-link" to="/">Go back to the homepage</Link>
+      </MainMatter>
+      <Footer bottomImage={false} />
+    </Layout>
+    )}
+  />
 );
 
 const MainMatter = styled.div`
