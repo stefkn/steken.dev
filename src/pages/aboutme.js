6026323a60f0ded8d7c2bfcedf709df884ee1a59
Stefan Nowak <stefan.nowak@tails.com>
2021-10-01 17:53:59 +0100

    implement new about me page

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -84,12 +84,149 @@ const AboutMe = () => (
     render={data => (
     <Layout>
       <Navbar />
-      <MainMatter className="page-main">
-        <Art><Img fluid={data.stefan_img.childImageSharp.fluid} /></Art>
-        <h1 className="title-main">about stefan</h1>
-        Human; human, after all.
-        <Link className="home-link" to="/">Go back to the homepage</Link>
-      </MainMatter>
+      <HeroImage><HeroOverlay></HeroOverlay><Img fluid={data.stefan_img.childImageSharp.fluid} /></HeroImage>
+      <Container>
+        <MainMatter>
+          <h1>About</h1>
+
+          <h3>Welcome to my site.</h3>
+
+          <p>This is the website of Stefan Kenichiro Nowak.</p>
+
+          <p>I'm a software engineer, currently living and working in London, England.</p>
+
+          <p>
+            I enjoy teamwork and solving problems collaboratively with people of varying levels of experience, domains of expertise, and from diverse social and cultural backgrounds.
+          </p>
+
+          <p>
+            I work best in environments that empower individuals with a high degree of autonomy, the freedom to make decisions and where people are valued partners in a journey–not work robots for hire.
+          </p>
+
+          <p>
+            My personal philosophy (specifically regarding software, and working together) is heavily inspired by the <a href="https://www.recurse.com/manual">User's Manual of the Recurse Center</a>. I especially like their <a href="https://www.recurse.com/manual#sec-environment">Social Rules</a>.
+          </p>
+
+          <p>
+            I was inspired to start writing articles and publishing them on a personal website by fellow engineers and friends: <a href="https://healeycodes.com/">healeycodes</a>, <a href="https://www.moonclash.com/">moonclash</a> (the adam sandler of software), <a href="https://samlader.com/">saml</a>, <a href="https://matteo.baldelli.dev/">matteo</a> and more.
+          </p>
+
+          <section class="section-tech">
+            <div class="section-tech-container">
+              <strong class="section-tech-title">My technology stack</strong>
+              <h2>Technologies I have used, and like to use</h2>
+              <div class="technology-stack">
+                <div class="row">
+
+                  <div class="col tech-stack-item-col">
+                    <div class="tech-stack-item-logo">
+                     <img src={data.python_logo.publicURL} />
+                    </div>
+                    <div class="stack-item-text">
+                      <h4>Python</h4>
+                      <p>I'm most experienced using Python. good code snek</p>
+                    </div>
+                  </div>
+
+                  <div class="col tech-stack-item-col">
+                    <div class="tech-stack-item-logo">
+                      <img src={data.flask_logo.publicURL} />
+                    </div>
+                    <div class="stack-item-text">
+                      <h4>Flask</h4>
+                      <p>I use a lot of Flask at work. Jinja, mmm. </p>
+                    </div>
+                  </div>
+
+                  <div class="col tech-stack-item-col">
+                    <div class="tech-stack-item-logo">
+                      <img src={data.vue_logo.publicURL} />
+                    </div>
+                    <div class="stack-item-text">
+                      <h4>Vue.js</h4>
+                      <p>We made a Vue.js microfrontend! </p>
+                    </div>
+                  </div>
+
+                  <div class="col tech-stack-item-col">
+                    <div class="tech-stack-item-logo">
+                      <img src={data.mysql_logo.publicURL} />
+                    </div>
+                    <div class="stack-item-text">
+                      <h4>MySQL</h4>
+                      <p>We use a MySQL DB at work. Still don't fully understand JOINS.</p>
+                    </div>
+                  </div>
+
+                  <div class="col tech-stack-item-col">
+                    <div class="tech-stack-item-logo">
+                      <img src={data.javascript_logo.publicURL} />
+                    </div>
+                    <div class="stack-item-text">
+                      <h4>JavaScript</h4>
+                      <p>I use a lot of JS in personal and work projects. NaN projects.</p>
+                    </div>
+                  </div>
+
+                  <div class="col tech-stack-item-col">
+                    <div class="tech-stack-item-logo">
+                      <img src={data.gatsby_logo.publicURL} />
+                    </div>
+                    <div class="stack-item-text">
+                      <h4>Gatsby</h4>
+                      <p>This site is made using Gatsby. I'm liking it so far.</p>
+                    </div>
+                  </div>
+
+                  <div class="col tech-stack-item-col">
+                    <div class="tech-stack-item-logo">
+                      <img src={data.java_logo.publicURL} />
+                    </div>
+                    <div class="stack-item-text">
+                      <h4>Java</h4>
+                      <p>Java was my first "proper" OOP language. public static void uuhh </p>
+                    </div>
+                  </div>
+
+                  <div class="col tech-stack-item-col">
+                    <div class="tech-stack-item-logo">
+                      <img src={data.react_logo.publicURL} />
+                    </div>
+                    <div class="stack-item-text">
+                      <h4>React</h4>
+                      <p>Are you even a developer these days without React on your CV?</p>
+                    </div>
+                  </div>
+
+                  <div class="col tech-stack-item-col">
+                    <div class="tech-stack-item-logo">
+                      <img src={data.graphql_logo.publicURL} />
+                    </div>
+                    <div class="stack-item-text">
+                      <h4>GraphQL</h4>
+                      <p>I learned a lot of GQL working with Gatsby. We also use it at work.</p>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <p>
+            While I work, I like to listen to concentration-boosting music, which I try to organise into Spotify playlists, if I'm feeling diligent. Take a listen!
+          </p>
+
+          <div class="spotify-container">
+            <iframe class="spotify-embed"
+            src="https://open.spotify.com/embed/playlist/33KRDnK7r5ni98VbD4fFcI?theme=1"
+            width="100%" height="375" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <iframe class="spotify-embed"
+            src="https://open.spotify.com/embed/playlist/7KgJoiKb4HKasiHp2r9xv8"
+            width="100%" height="375" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+          </div>
+        </MainMatter>
+      </Container>
       <Footer bottomImage={false} />
     </Layout>
     )}
