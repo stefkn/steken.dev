51c114757ceb23fb0950ff98a77cd8581416f8e8
Stefan Nowak <stefan.nowak@tails.com>
2022-07-14 17:52:20 +0100

    add in Adobe Document Cloud View SDK script via html.js

---
+++ src/html.js
@@ -0,0 +1,41 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+// This file defines a top-level function which determines
+// DOM elements included on every page of the site
+// We use it to include the Adobe Document Cloud View SDK script
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+        <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
