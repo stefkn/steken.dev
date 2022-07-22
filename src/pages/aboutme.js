75ad45f9fe74daeec2abcc6a57f04ac446786c31
Stefan Nowak <stefan.nowak@tails.com>
2022-07-22 10:16:18 +0100

    eslint imports

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StaticQuery, graphql, Link } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
 import Layout from '@common/Layout';
