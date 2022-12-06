44c1320fcfc4fa760278a4bbf8282e30b1fd29a7
Stefan Nowak <stefan.nowak@tails.com>
2022-12-06 23:50:34 +0000

    add RecentPosts functions

--- src/components/sections/RecentPosts.js
+++ src/components/sections/RecentPosts.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
+import { StaticQuery, graphql } from 'gatsby'
 
 import PostLink from "../common/PostLink"
 
 class RecentPosts extends Component {
+  isArticlePublished(article) {
+    return article.node.frontmatter.published
+  }
+
+  getCoverImage(allImages, coverImageName) {
+    let filteredImages = allImages.filter(image => this.isSameImageName(image, coverImageName))
+    // if multiple are matched, return the first one
+    return (filteredImages.length > 0) ? filteredImages[0].node : null
+  }
+
+  isSameImageName(image, coverImageName) {
+    return image.node.childImageSharp.fluid.originalName === coverImageName
+  }
+
   render() {
     return (
       <RecentPostsDiv id="recent-articles">
